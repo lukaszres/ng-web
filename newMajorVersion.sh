1acc6a9b1188534d281d7d4e8f437b6038b6c998
lkre <lukasz.res@gmail.com>
2022-01-11 16:51:37 +0100

    #7 display application version on the footer

---
+++ newMajorVersion.sh
@@ -0,0 +1,1 @@
+npm --no-git-tag-version version major
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'ng-web';
   fileName: string = '';
+  currentApplicationVersion: string = environment.appVersion;
 
   changeArticle(fileName: string) {
     this.fileName = fileName;
